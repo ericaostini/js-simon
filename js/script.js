2133ab95001383c4f5b93018fda6551445435bde
ericaostini <ericaostini14@gmail.com>
2023-10-20 15:27:09 +0200

    generate random number with function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const numGenerate = 5;
+const arrayRandom = [];
+const min = 1;
+const max = 100;
+
+let randomNum = getRndInteger(min, max)
+console.log(randomNum);
+// while (arrayRandom.length < numGenerate){
+// }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
