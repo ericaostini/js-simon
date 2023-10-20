dc9555ae2fc86022c13f9c0e75b17e402b09f290
ericaostini <ericaostini14@gmail.com>
2023-10-20 15:29:22 +0200

    while loop to insert randomNum into theRandom array

--- js/script.js
+++ js/script.js
@@ -5,10 +5,13 @@ const arrayRandom = [];
 const min = 1;
 const max = 100;
 
-let randomNum = getRndInteger(min, max)
-console.log(randomNum);
-// while (arrayRandom.length < numGenerate){
-// }
+while (arrayRandom.length < numGenerate){
+    let randomNum = getRndInteger(min, max)
+    console.log(randomNum);
+    if (!arrayRandom.includes(randomNum)){
+        arrayRandom.push(randomNum);
+    }
+}
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
