7c0e519f31c50c1494d0797b3dd0d47ccb244158
ericaostini <ericaostini14@gmail.com>
2023-10-20 15:33:12 +0200

    display arraRandom in html

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const numGenerate = 5;
 const arrayRandom = [];
 const min = 1;
 const max = 100;
+const numberBox = document.getElementById("box");
 
 while (arrayRandom.length < numGenerate){
     let randomNum = getRndInteger(min, max)
@@ -11,7 +12,9 @@ while (arrayRandom.length < numGenerate){
     if (!arrayRandom.includes(randomNum)){
         arrayRandom.push(randomNum);
     }
+    
 }
+numberBox.innerHTML = arrayRandom;
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
